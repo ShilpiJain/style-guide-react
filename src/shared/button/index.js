f2302ecac06a23afcf899c087c8757f781017d4e
shilpi_cars24 <shilpi@Mohds-MacBook-Pro.local>
2019-09-19 16:55:13 +0530

    button

--- src/shared/button/index.js
+++ src/shared/button/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import "./style.sass";
 
 const Button = () => {
     return (
         <div>
             <h3 className="heading">Buttons - <span></span></h3>
             <p>Buttons used in Cars24 site.</p>
+                <div className="block col-md-6">
+                    <h6>Primary Button</h6>
+                    <button className="cars24-btn">SELL MY CAR</button>
+                    <h6>Secondary Button</h6>
+                    <button className="cars24-sec-btn">SELL MY CAR</button>
+                    <h6>Disable Button</h6>
+                    <button className="cars24-btn-disable">SELL MY CAR</button>
+                    <h6>Secondary Disable Button</h6>
+                    <button className="cars24-sec-btn disable">SELL MY CAR</button>
+                </div>
         </div>
     )
 }
--- src/shared/typography/index.js
+++ src/shared/typography/index.js
@@ -3,15 +3,31 @@ import "./style.sass";
 
 const Typography = () => {
     return (
-        <div>
+        <div className="typo">
            <h3 className="heading">Typography - <span>Style</span></h3> 
            <p>Typographic style used in Cars24 site.</p>
-           <h1>H1 - 40px, Roboto (500), line-height: 1.2;, </h1>
-           <h2>H2 - 24px, Open Sans, Light (300)</h2>
-           <h3>H3 - 18px, Open Sans, Light (300)</h3>
-           <h4>H4 - 15px, Open Sans, Light (300)</h4>
-           <h5>H5 - 14px, Open Sans, Light (300)</h5>
-           <h6>H6 - 12px, Open Sans, Light (300)</h6>
+            <div className="dark">
+                <h1>H1 - 36px, Roboto-Medium (500), 48px Dark</h1>
+                <h2>H2 - 24px, Roboto-Medium (500), 32px </h2>
+                <h3>H3 - 20px, Roboto-Medium (500), 32px </h3>
+                <h4>H4 - 16px, Roboto (500), Dark </h4>
+                <h5>H5 - 14px, Roboto (500), Dark </h5>
+                <h6>H6 - 12px, Roboto (500), Dark </h6>
+            </div>
+            <div className="light">
+                <h1>H1 - 28px, Roboto (300), light</h1>
+                <h2>H2 - 24px, Roboto (300), light </h2>
+                <h3>H3 - 18px, Roboto (300), light </h3>
+                <h4>H4 - 15px, Roboto (300), light </h4>
+                <h5>H5 - 14px, Roboto (300), light </h5>
+                <h6>H6 - 12px, Roboto (300), light  </h6>
+            </div>
+            <div className="light">
+                <p className="para-dark">Paragraph dark, 16px, (500), 20px</p>
+                <p className="para-light">Paragraph dark, 16px, (500) 20px</p>
+                <p className="para-orange">Paragraph orange, Roboto-Regular 16px, (500) 20px</p>
+                <p className="para-blue">Paragraph blue, Roboto-Regular 16px, (500) 20px</p>
+            </div>
         </div>
     )
 }
